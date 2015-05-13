21f48c0b07e4b3a6b9daac37ab41e8857bb9dded
Michael Frister <michael.frister@barzahlen.de>
2015-05-13 10:52:48 +0200

    Text strategy: Also translate numbers

--- spec/lib/tidus/strategies/postgresql/text_anonymizer_spec.rb
+++ spec/lib/tidus/strategies/postgresql/text_anonymizer_spec.rb
@@ -5,8 +5,8 @@ require 'spec_helper'
 describe Tidus::Postgresql::TextAnonymizer do
   it "returns an SQL statement with a randomly generated mapping" do
     result = described_class.anonymize('foo', 'bar')
-    base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZCßüäöÜÄÖ"
+    base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZCßüäöÜÄÖ0123456789"
     result.should match(
-      /translate\(\(foo\.bar\)::text, '#{base}'::text, '[A-Za-z0-9ÄÖÜäöü]{60}'::text\)/)
+      /translate\(\(foo\.bar\)::text, '#{base}'::text, '[A-Za-z0-9ÄÖÜäöü]{70}'::text\)/)
   end
 end
